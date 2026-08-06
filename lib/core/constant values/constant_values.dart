b1e13a27c94301c0a63c6951d41318338c4a48bb
Amro27code <amrosramrosr05@gmail.com>
2026-08-06 11:41:09 +0300

    add font AA-GALAXY and Add flutter_svg package

--- lib/core/constant values/constant_values.dart
+++ lib/core/constant values/constant_values.dart
@@ -10,26 +10,30 @@ class ConstListValues {
     ),
     OnBoardingModel(
       imagePath: AssetsValueManager.onb2,
-      title: ConstValues.onbTitle1,
+      title: ConstValues.onbTitle2,
     ),
     OnBoardingModel(
       imagePath: AssetsValueManager.onb3,
-      title: ConstValues.onbTitle1,
+      title: ConstValues.onbTitle3,
     ),
     OnBoardingModel(
       imagePath: AssetsValueManager.onb4,
-      title: ConstValues.onbTitle1,
+      title: ConstValues.onbTitle4,
     ),
     OnBoardingModel(
       imagePath: AssetsValueManager.onb5,
-      title: ConstValues.onbTitle1,
+      title: ConstValues.onbTitle5,
     ),
   ];
 }
-class ConstValues{
-static const String onbTitle1='يمكنك إضافة  بعض المراحل التعليمية ';
-static const String onbTitle2='يمكنك إضافة  بعض المراحل التعليمية ';
-static const String onbTitle3='يمكنك إضافة  بعض المراحل التعليمية ';
-static const String onbTitle4='يمكنك إضافة  بعض المراحل التعليمية ';
-static const String onbTitle5='يمكنك إضافة  بعض المراحل التعليمية ';
+
+class ConstValues {
+  static const String onbTitle1 = 'يمكنك إضافة  بعض المراحل التعليمية ';
+  static const String onbTitle2 =
+      'يمكنك إضافة  بعض المجموعات لكل مرحلة من المراحل التعليمية';
+  static const String onbTitle3 =
+      'يمكنك إضافة  بعض الطلاب لكل جروب الموجودة في كل مرحلة تعليمية';
+  static const String onbTitle4 = 'يمكنك إضافة  حضور وغياب لكل طالب ';
+  static const String onbTitle5 =
+      'يمكنك إضافة  ما إذا كان الطالب دفع هذا الشهر أم لا وإضافة تاريخ الدفع';
 }
--- lib/view/on_boarding/screens/on_boarding_screen.dart
+++ lib/view/on_boarding/screens/on_boarding_screen.dart
@@ -1,10 +1,38 @@
+import 'package:drosak_managment_app/core/constant%20values/constant_values.dart';
+import 'package:drosak_managment_app/core/resources/assets_manager.dart';
+import 'package:drosak_managment_app/core/resources/color_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("data")));
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SizedBox(
+          width: .infinity,
+          child: Column(
+            spacing: 60,
+            mainAxisAlignment: .center,
+            crossAxisAlignment: .center,
+            children: [
+              SvgPicture.asset(AssetsValueManager.onb1, height: 200),
+              Text(
+                ConstListValues.onBoardingModel[0].title,
+                textAlign: .center,
+                style: TextStyle(
+                  fontFamily: "AA-GALAXY",
+                  fontSize: 20,
+                  color: ColorManager.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
